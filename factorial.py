c25b361889f10934b65a96a4abc1134e96477bf7
vinay <vinaykumar06061998.vk@gmail.com>
2020-01-29 00:34:31 +0530

    this is my homework

---
+++ factorial.py
@@ -0,0 +1,15 @@
+num=8
+#to take input from user
+#num=int(input("enter a numbers:"))
+factorial=1
+#to check the number is positive or negative.
+if num<0:
+    print("the factorial of negative numbers is not exist")
+elif num==0:
+    print("the factorial is 0 is 1")
+    for i in range (1,num):
+        factorial=factorial*i
+        print("the factorial of num is",factorial)
+
+
+
---
+++ greater_number.py
@@ -0,0 +1,14 @@
+num1=4
+num2=7
+num3=9
+#num1=float(input("enter first number: "))
+#num2=float(input("enter second number: "))
+#num3=float(input("enter third number: "))
+if (num1>=num2) and (num1>=num3):
+    largest=num1
+elif (num2>=num1) and (num2>=num3):
+    largest=num2
+else:
+    largest=num3
+
+    print("the largest number is",largest)
